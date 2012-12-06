8140192a48e88a089296230d79829b8f137da603
Grace Muzny <muznyg@uw.edu>
2012-12-06 01:39:45 -0800

    Worked on #42 some more - Added validation to custom word lists, added instructions to custom word lists page

--- tiletothetop/static/js/wordlists.js
+++ tiletothetop/static/js/wordlists.js
@@ -44,8 +44,39 @@ function displayForms(data) {
 
 // Validate(?), push to server
 function saveList() {
-    $('#custom-form').attr('action', '/save-customlist/');
-    $('#custom-form').submit();
+    // Check to make sure that all words are only composed of
+    // characters that we support.
+    var pattern = new RegExp("^[a-zA-Z 0-9]+$");
+    var list = $("#list-entries").children();
+    var okay = true;
+    var count = 0;
+    // go to length - 1 because the add row box is at the end of this list
+    for (var i = 0; i < list.length - 1; i++) {
+	if ($(list[i]).is(":visible")) {
+	    var input = $($(list[i]).children()[2]).children();
+	    var text = input.val(); // dependent on the form of the form
+	    if (!pattern.test(text) || text.length > 10) {
+		okay = false;
+		input.css("border", "2px solid red");
+	    } else {
+		input.css("border", "2px solid white");
+	    }
+	    // There also must be a definition present
+	    var def = $($(list[i]).children()[4]).children();
+	    var defText = def.val();
+	    if (defText.length == 0) {
+		okay = false;
+		def.css("border", "2px solid red");
+	    } else {
+		def.css("border", "2px solid white");
+	    }
+	    count += 1;
+	}
+    }
+    if (okay && count >= 4) {
+	$('#custom-form').attr('action', '/save-customlist/');
+	$('#custom-form').submit();
+    }
 }
 
 // If list already exists, request to delete
