1f40e1a6a519a681d20494b01055e9a64b919a02
Justin McManus <jmcmanu@gmail.com>
2012-12-07 18:26:54 -0800

    Added monitoring and db backup Heroku apps. Added fab tasks for each.

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,11 @@
 from fabric.api import *
 from heroku_settings import *
 
+HEROKU_ADDONS = (
+    'pgbackups',            # db backups
+    'newrelic:standard'     # monitoring
+)
+
 HEROKU_CONFIGS = (
     'FB_APP_ID=%s' % FB_APP_ID,
     'FB_API_SECRET=%s' % FB_API_SECRET,
@@ -10,24 +15,45 @@ HEROKU_CONFIGS = (
 )
 
 def deploy():
+    """Deploy the app to Heroku"""
+    backup()
     local('git push heroku master -f')
     local('heroku run ./manage.py syncdb')
     local('heroku run ./manage.py migrate')
 
 def test():
+    """Run tests"""
     local('./manage.py test tiletothetop')
 
 def bootstrap():
+    """Bootstrap and deploy a fresh Heroku app"""
+    for addon in HEROKU_ADDONS:
+        cont('heroku addons:add %s' %addon,
+             "Couldn't add %s to your Heroku app, continue anyway?" %config)
+
     for config in HEROKU_CONFIGS:
         cont('heroku config:add %s' % config,
-            "Couldn't add %s to your Heroku app, continue anyway?" % config)
+             "Couldn't add %s to your Heroku app, continue anyway?" % config)
 
     deploy()
 
+def backup():
+    """Backup the live database"""
+    local('heroku pgbackups:capture')
+
+def view_backups():
+    """Show the available database backups"""
+    local('heroku pgbackups')
+    print('To restore:  heroku pgbackups:restore [db to restore to] [backup id]')
+
+def monitor():
+    """Launch the url of the monitoring app"""
+    local('heroku addons:open newrelic')
+
 # Helper function
 def cont(cmd, message):
-    """Given a command, ``cmd``, and a message, ``message``, allow a user to
-    either continue or break execution if errors occur while executing ``cmd``.
+    """Given a command, `cmd`, and a message, `message`, allow a user to
+    either continue or break execution if errors occur while executing `cmd`.
     """
     with settings(warn_only=True):
         result = local(cmd, capture=True)
